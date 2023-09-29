0ac4124419feae9eda305332217b3a99fce15bcd
Manandhar <sudip.manandhar@cotiviti.com>
2023-09-29 14:18:22 +0545

    Minor changes | Simple Security & Database Config

---
+++ src/main/java/com/cotiviti/Pasaw/security/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package com.cotiviti.Pasaw.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration              // what is this?
+@EnableWebSecurity          // what is this?
+public class SecurityConfiguration {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .cors(AbstractHttpConfigurer::disable)
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .formLogin(form -> form.disable())
+        ;
+        return http.build();
+    }
+
+
+}
