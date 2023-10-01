59aaf9d158029abf5ccb5361e061e995cfc4fee6
Manandhar <sudip.manandhar@cotiviti.com>
2023-10-01 10:21:25 +0545

    Minor Changes | H2 DB setup done

--- src/main/java/com/cotiviti/Pasaw/controller/HomeController.java
+++ src/main/java/com/cotiviti/Pasaw/controller/HomeController.java
@@ -3,10 +3,13 @@ package com.cotiviti.Pasaw.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("")
+    @GetMapping("/")
     public String greeting(){
         return "<h1>Welcome to Pa:Saw</h1>";
     }
--- src/main/java/com/cotiviti/Pasaw/security/SecurityConfiguration.java
+++ src/main/java/com/cotiviti/Pasaw/security/SecurityConfiguration.java
@@ -7,22 +7,22 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
-@Configuration              // what is this?
-@EnableWebSecurity          // what is this?
+@Configuration // what is this?
+@EnableWebSecurity // what is this?
 public class SecurityConfiguration {
-
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .cors(AbstractHttpConfigurer::disable)
-            .csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .formLogin(form -> form.disable())
-        ;
+            .authorizeHttpRequests(auth -> auth.requestMatchers(toH2Console()).permitAll())
+            .headers(headers -> headers.frameOptions().disable())
+            .csrf(csrf -> csrf.ignoringRequestMatchers(toH2Console()));
         return http.build();
     }
-
 
 }
